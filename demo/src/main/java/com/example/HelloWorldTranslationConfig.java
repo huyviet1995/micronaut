9c27e3405a40a663fde785865223cf61ae1a3d3d
Viet Dang <viet.dang@dirox.net>
2022-01-23 21:27:40 +0700

    Adding NotBlank

--- demo/src/main/java/com/example/HelloWorldTranslationConfig.java
+++ demo/src/main/java/com/example/HelloWorldTranslationConfig.java
@@ -2,10 +2,14 @@ package com.example;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
 
+import javax.validation.constraints.NotBlank;
+
 @ConfigurationProperties("hello.world.translation")
 public interface HelloWorldTranslationConfig {
 
+    @NotBlank
     String getDe();
 
+    @NotBlank
     String getEn();
 }
