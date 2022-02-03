e88e7c4c4e7c8be0448cec08d25af5b864daa8f2
Viet Dang <viet.dang@dirox.net>
2022-02-03 16:45:17 +0700

    Create the RestAPIResponse

--- demo/src/main/java/com/example/broker/wallet/error/CustomError.java
+++ demo/src/main/java/com/example/broker/wallet/error/CustomError.java
@@ -1,8 +1,10 @@
-package com.example.broker.wallet;
+package com.example.broker.wallet.error;
+
+import com.example.broker.api.RestApiResponse;
 
 public record CustomError(
         int status,
         String error,
         String message
-) {
+) implements RestApiResponse {
 }
