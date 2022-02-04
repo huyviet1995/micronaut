1f90ef5a70af12e254d36df3b6bc4c03c1e9782e
Viet Dang <viet.dang@dirox.net>
2022-02-04 12:40:09 +0700

    Add CustomErrorResponseProcessor

---
+++ demo/src/main/java/com/example/broker/wallet/error/CustomErrorResponseProcessor.java
@@ -0,0 +1,32 @@
+package com.example.broker.wallet.error;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.server.exceptions.response.ErrorContext;
+import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class CustomErrorResponseProcessor implements ErrorResponseProcessor<CustomError> {
+
+    @Override
+    public MutableHttpResponse<CustomError> processResponse(@NonNull ErrorContext errorContext, @NonNull MutableHttpResponse<?> response) {
+        CustomError customError;
+        if (!errorContext.hasErrors()) {
+            customError = new CustomError(
+                    response.getStatus().getCode(),
+                    response.getStatus().name(),
+                    "No custom errors found"
+            );
+        } else {
+            var firstError = errorContext.getErrors().get(0);
+            customError = new CustomError(
+                    response.getStatus().getCode(),
+                    response.getStatus().name(),
+                    firstError.getMessage()
+            );
+        }
+        return response.body(customError).contentType(MediaType.APPLICATION_JSON);
+    }
+}
