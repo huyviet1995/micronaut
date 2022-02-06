59492ec134b42d5966064987352d367ffaf82b5b
Viet Dang <viet.dang@dirox.net>
2022-02-06 17:04:01 +0700

    Start basic reactive Java

--- src/main/java/com/example/broker/WatchListControllerReactive.java
+++ src/main/java/com/example/broker/WatchListControllerReactive.java
@@ -5,30 +5,39 @@ import com.example.broker.store.InMemoryAccountStore;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
-@Controller("/account/watchlist")
-public class WatchListController {
+@Controller("/account/watchlist-reactive")
+public class WatchListControllerReactive {
 
     private final InMemoryAccountStore store;
     public static final UUID ACCOUNT_ID = UUID.randomUUID();
+    private static final Logger LOG = LoggerFactory.getLogger(WatchListControllerReactive.class);
 
-    public WatchListController(final InMemoryAccountStore store) {
+    public WatchListControllerReactive(final InMemoryAccountStore store) {
         this.store = store;
     }
 
     @Get("/")
+    @ExecuteOn(TaskExecutors.IO)
     public HttpResponse<WatchList> get() {
+        LOG.debug("Get watchlist {}", Thread.currentThread().getName());
         return HttpResponse.ok().body(store.getWatchList(ACCOUNT_ID));
     }
 
     @Put(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
     public WatchList update(@Body WatchList watchList) {
         return store.updateWatchList(ACCOUNT_ID, watchList);
     }
 
     @Delete(value="/{accountId}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
     public void delete(@PathVariable UUID accountId) {
        store.deleteWatchList(accountId);
     }
--- src/test/java/com/example/WatchListControllerReactiveTest.java
+++ src/test/java/com/example/WatchListControllerReactiveTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import com.example.broker.WatchListController;
+import com.example.broker.WatchListControllerReactive;
 import com.example.broker.model.Symbol;
 import com.example.broker.model.WatchList;
 import com.example.broker.store.InMemoryAccountStore;
@@ -10,31 +10,35 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.rxjava2.http.client.RxHttpClient;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableConverter;
+import io.reactivex.Single;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.spockframework.util.Assert;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @MicronautTest
-public class WatchListControllerTest {
+public class WatchListControllerReactiveTest {
     private static final UUID ACCOUNT_ID = UUID.randomUUID();
-    private static final UUID TEST_ACCOUNT_ID = WatchListController.ACCOUNT_ID;
+    private static final UUID TEST_ACCOUNT_ID = WatchListControllerReactive.ACCOUNT_ID;
 
     @Inject EmbeddedApplication application;
 
-    @Inject @Client("/account") HttpClient client;
+    @Inject @Client("/account")
+    RxHttpClient client;
 
     @Inject InMemoryAccountStore store;
 
     @Test
     void returnsEmptyWatchListForAccount() {
-        final WatchList result = client.toBlocking().retrieve("/watchlist", WatchList.class);
-        Assertions.assertTrue(result.getSymbols().isEmpty());
+        final Single<WatchList> result = client.retrieve(HttpRequest.GET("/watchlist-reactive"), WatchList.class).singleOrError();
+        Assertions.assertTrue(result.blockingGet().getSymbols().isEmpty());
         Assertions.assertTrue(store.getWatchList(TEST_ACCOUNT_ID).getSymbols().isEmpty());
     }
 
@@ -44,7 +48,7 @@ public class WatchListControllerTest {
         WatchList watchList = new WatchList(symbols);
         store.updateWatchList(TEST_ACCOUNT_ID, watchList);
 
-        final WatchList result = client.toBlocking().retrieve("/watchlist", WatchList.class);
+        final WatchList result = client.toBlocking().retrieve("/watchlist-reactive", WatchList.class);
         Assertions.assertEquals(3, result.getSymbols().size());
         Assertions.assertEquals(3, store.getWatchList(TEST_ACCOUNT_ID).getSymbols().size());
     }
@@ -54,7 +58,7 @@ public class WatchListControllerTest {
         final var symbols = Stream.of("APPL", "AMZN", "GOOGL").map(Symbol::new).collect(Collectors.toList());
         WatchList watchList = new WatchList(symbols);
 
-        final HttpResponse<Object> response = client.toBlocking().exchange(HttpRequest.PUT("/watchlist", watchList));
+        final HttpResponse<Object> response = client.toBlocking().exchange(HttpRequest.PUT("/watchlist-reactive", watchList));
         Assertions.assertEquals(HttpStatus.OK, response.getStatus());
         Assertions.assertEquals(watchList, store.getWatchList(TEST_ACCOUNT_ID));
     }
@@ -65,7 +69,7 @@ public class WatchListControllerTest {
         WatchList watchList = new WatchList(symbols);
         store.updateWatchList(TEST_ACCOUNT_ID, watchList);
 
-        final HttpResponse<Object> response = client.toBlocking().exchange(HttpRequest.DELETE("/watchlist/" + TEST_ACCOUNT_ID));
+        final HttpResponse<Object> response = client.toBlocking().exchange(HttpRequest.DELETE("/watchlist-reactive/" + TEST_ACCOUNT_ID));
         Assertions.assertEquals(HttpStatus.OK, response.getStatus());
         Assertions.assertTrue(store.getWatchList(TEST_ACCOUNT_ID).getSymbols().isEmpty());
     }
