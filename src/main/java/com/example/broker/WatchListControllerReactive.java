6f0ac19c563d2e38df32bec896d4f19ea7b89652
Viet Dang <viet.dang@dirox.net>
2022-02-06 17:45:58 +0700

    Subscribe on the scheduler thread
    This means that I will subscribe on the scheduler thread => perform some I/O operation (In this example, store.getWatchList(ACCOUNT_ID) and output the result in that thread.

--- src/main/java/com/example/broker/WatchListControllerReactive.java
+++ src/main/java/com/example/broker/WatchListControllerReactive.java
@@ -7,10 +7,15 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+import jakarta.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.reactivex.Single;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 @Controller("/account/watchlist-reactive")
 public class WatchListControllerReactive {
@@ -18,9 +23,21 @@ public class WatchListControllerReactive {
     private final InMemoryAccountStore store;
     public static final UUID ACCOUNT_ID = UUID.randomUUID();
     private static final Logger LOG = LoggerFactory.getLogger(WatchListControllerReactive.class);
+    private final Scheduler scheduler;
 
-    public WatchListControllerReactive(final InMemoryAccountStore store) {
+    public WatchListControllerReactive(final InMemoryAccountStore store, @Named(TaskExecutors.IO) ExecutorService executorService) {
         this.store = store;
+        this.scheduler = Schedulers.from(executorService);
+    }
+
+    @Get(value="/single", produces = MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
+    public Single<WatchList> getAsSingle() {
+        LOG.debug("getAsSingle {}", Thread.currentThread().getName());
+        return Single.fromCallable(() -> {
+            LOG.debug("getAsSingle {}", Thread.currentThread().getName());
+            return store.getWatchList(ACCOUNT_ID);
+        }).subscribeOn(scheduler);
     }
 
     @Get("/")
--- src/test/java/com/example/WatchListControllerReactiveTest.java
+++ src/test/java/com/example/WatchListControllerReactiveTest.java
@@ -18,7 +18,10 @@ import io.reactivex.Single;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,6 +37,7 @@ public class WatchListControllerReactiveTest {
     RxHttpClient client;
 
     @Inject InMemoryAccountStore store;
+    private static final Logger LOG = LoggerFactory.getLogger(WatchListControllerReactive.class);
 
     @Test
     void returnsEmptyWatchListForAccount() {
@@ -54,6 +58,18 @@ public class WatchListControllerReactiveTest {
     }
 
     @Test
+    void returnsWatchListForAccountAsSingle() {
+        final var symbols = Stream.of("APPL", "AMZN", "GOOGL").map(Symbol::new).collect(Collectors.toList());
+        WatchList watchList = new WatchList(symbols);
+        store.updateWatchList(TEST_ACCOUNT_ID, watchList);
+
+        final Single<WatchList> result = client.retrieve(HttpRequest.GET("/watchlist-reactive/single"), WatchList.class).singleOrError();
+        LOG.debug(result.blockingGet().getSymbols().toString());
+        Assertions.assertEquals(3, result.blockingGet().getSymbols().size());
+        Assertions.assertEquals(3, store.getWatchList(TEST_ACCOUNT_ID).getSymbols().size());
+    }
+
+    @Test
     void canUpdateWatchListForAccount() {
         final var symbols = Stream.of("APPL", "AMZN", "GOOGL").map(Symbol::new).collect(Collectors.toList());
         WatchList watchList = new WatchList(symbols);
@@ -62,6 +78,7 @@ public class WatchListControllerReactiveTest {
         Assertions.assertEquals(HttpStatus.OK, response.getStatus());
         Assertions.assertEquals(watchList, store.getWatchList(TEST_ACCOUNT_ID));
     }
+
 
     @Test
     void canDeleteWatchlistForAccount() {
